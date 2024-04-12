ff7010f07a8ae4d0ab81991e3c433a221c50b638
Jasmine Ong <jon25@uclive.ac.nz>
2024-04-12 12:29:05 +1200

    Fixed the issue in security configurations and now forgot password link directs the user to the right page. Still very buggy due to Spring Security though so will need to fix that.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LostPasswordFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LostPasswordFormController.java
@@ -1,8 +1,10 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class LostPasswordFormController {
     @GetMapping("/forgotPassword")
     public String getLostPasswordForm() {
