f120d63728cb0844c0b8c8bb02dae55f0c6aad47
Jasmine Ong <jon25@uclive.ac.nz>
2024-04-18 16:56:55 +1200

    Made reset password form with a "new password" field and a "retype password" field. Will work on linking it to the right url soon.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LostPasswordFormController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LostPasswordFormController.java
@@ -13,6 +13,6 @@ public class LostPasswordFormController {
 
     @PostMapping("/resetPassword")
     public String sendResetPasswordLink() {
-        return "lostPasswordForm";
+        return "newPasswordForm";
     }
 }
