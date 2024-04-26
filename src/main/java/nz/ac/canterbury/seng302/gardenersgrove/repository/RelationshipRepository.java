2465b2e3249de046499d4c3bf86e8abf5637ef83
Sam Dawson <sda110@uclive.ac.nz>
2024-04-26 16:19:09 +1200

    Added link to manage friends to the nav bar

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/RelationshipRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/RelationshipRepository.java
@@ -22,9 +22,9 @@ public interface RelationshipRepository extends CrudRepository<Relationships, Lo
      * @param id
      * @returns a list of all the accepted relationships of the user
      */
-    @Query(value = "SELECT gardener_id FROM Relationship WHERE friend_id = ?1 AND status='accepted' " +
+    @Query(value = "SELECT gardener_id FROM Relationships WHERE friend_id = ?1 AND status='accepted' " +
             "UNION " +
-            "SELECT friend_id FROM Relationship WHERE gardener_id=?1 AND status='accepted'", nativeQuery = true)
+            "SELECT friend_id FROM Relationships WHERE gardener_id=?1 AND status='accepted'", nativeQuery = true)
     List<Long> getGardenerFriends(Long id);
 
     @Query(value = "Select friend_id FROM Relationships WHERE gardener_id = ?1 and status = 'pending'", nativeQuery = true)
