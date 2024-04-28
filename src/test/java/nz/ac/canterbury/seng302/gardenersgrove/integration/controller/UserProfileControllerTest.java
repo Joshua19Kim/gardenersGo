e989178a2f1a935beac5702dc1bdca87ba462616
Joshua Kim <bki42@uclive.ac.nz>
2024-04-28 23:56:17 +1200

    created Integration test of UserprofileController and passwordUpdating function. Still in progress.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/UserProfileControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/UserProfileControllerTest.java
@@ -1,8 +1,5 @@
 package nz.ac.canterbury.seng302.gardenersgrove.integration.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import nz.ac.canterbury.seng302.gardenersgrove.controller.RegisterController;
 import nz.ac.canterbury.seng302.gardenersgrove.controller.UserProfileController;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Gardener;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenerFormService;
@@ -10,31 +7,25 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.ImageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
+import org.mockito.Captor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
-import java.awt.*;
 import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserProfileController.class)
@@ -48,26 +39,40 @@ public class UserProfileControllerTest {
     @MockBean
     private AuthenticationManager authenticationManager;
     @MockBean
+    private Optional gardenerOptional;
+    @MockBean
     private Authentication authentication;
     @MockBean
     private ImageService imageService;
+    @MockBean
+    private Gardener testGardener;
+    @MockBean
+    private UsernamePasswordAuthenticationToken token;
 
     @BeforeEach
     void setUp() {
         //Use this following gardener as the one called from db.
-        Gardener testGardener = new Gardener(
+        testGardener = new Gardener(
                 "testFirstName",
                 "testLastName",
                 LocalDate.of(1980, 1, 1),
                 "testEmail@gmail.com",
                 "testPassword",
                 "testProfilePhoto.jpg");
-        Mockito.when(gardenerFormService.findByEmail(anyString())).thenReturn(Optional.of(testGardener));
+//        gardenerFormService = Mockito.mock(GardenerFormService.class);
+//        gardenerFormService.addGardener(testGardener);
+//        authenticationManager = Mockito.mock(AuthenticationManager.class);
+//        token = new UsernamePasswordAuthenticationToken(testGardener.getEmail(), testGardener.getPassword());
+//        authentication = authenticationManager.authenticate(token);
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        Mockito.when(SecurityContextHolder.getContext().getAuthentication()).thenReturn(authentication);
+        gardenerFormService.addGardener(testGardener);
+        when(gardenerFormService.findByEmail("testEmail@gmail.com")).thenReturn(Optional.of(testGardener));
 
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser("testEmail@gmail.com")
     void onUserPage_userWantsToSeeDetails_showCorrectDetails() throws Exception {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
@@ -81,8 +86,9 @@ public class UserProfileControllerTest {
                 .andExpect(model().attribute("profilePic", "testProfilePhoto.jpg"));
 
     }
+
     @Test
-    @WithMockUser
+    @WithMockUser("testEmail@gmail.com")
     void onUserPage_userChangesFirstNameWithInvalidFirstName_errorMessageProvided() throws Exception {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
@@ -97,11 +103,11 @@ public class UserProfileControllerTest {
                 .andExpect(view().name("user"))
                 .andExpect(model().attribute("firstNameValid", "First name cannot be empty and must only include letters, spaces, " +
                         "hyphens or apostrophes"));
-        }
+    }
 
 
     @Test
-    @WithMockUser
+    @WithMockUser("testEmail@gmail.com")
     void onUserPage_userChangesLastNameWithInvalidLastName_errorMessageProvided() throws Exception {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
@@ -117,8 +123,9 @@ public class UserProfileControllerTest {
                 .andExpect(model().attribute("lastNameValid", "Last name cannot be empty and must only include letters, spaces, " +
                         "hyphens or apostrophes"));
     }
+
     @Test
-    @WithMockUser
+    @WithMockUser("testEmail@gmail.com")
     void onUserPage_userChangesDoBWithTooLowDoB_errorMessageProvided() throws Exception {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
@@ -133,8 +140,9 @@ public class UserProfileControllerTest {
                 .andExpect(view().name("user"))
                 .andExpect(model().attribute("DoBValid", "You must be 13 years or older to create an account"));
     }
+
     @Test
-    @WithMockUser
+    @WithMockUser("testEmail@gmail.com")
     void onUserPage_userChangesDoBWithTooHighDoB_errorMessageProvided() throws Exception {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
@@ -151,7 +159,7 @@ public class UserProfileControllerTest {
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser("testEmail@gmail.com")
     void onUserPage_userChangesEmailWithInvalidEmail_errorMessageProvided() throws Exception {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
@@ -166,46 +174,84 @@ public class UserProfileControllerTest {
                 .andExpect(view().name("user"))
                 .andExpect(model().attribute("emailValid", "Email address must be in the form ‘jane@doe.nz"));
     }
+
     @Test
-    @WithMockUser
-    void onUserPage_userChangesFirstNameWithValidName_SaveNewFirstName() throws Exception {
+    @WithMockUser("testEmail@gmail.com")
+    void onUserPage_userChangesFirstNameWithValidName_NoErrorProvidedAndSaveNewFirstName() throws Exception {
+        testGardener = new Gardener(
+                "testFirstName",
+                "testLastName",
+                LocalDate.of(1980, 1, 1),
+                "testEmail@gmail.com",
+                "testPassword",
+                "testProfilePhoto.jpg");
+        gardenerFormService = Mockito.mock(GardenerFormService.class);
+        when(gardenerFormService.findByEmail(anyString())).thenReturn(Optional.of(testGardener));
+
+//        UserProfileController userProfileController = new UserProfileController(gardenerFormService);
+//        MockMvc MOCK_MVC = MockMvcBuilders.standaloneSetup(userProfileController).build();
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/user")
-                        .param("firstName", "newTestFirstName")
+                        .param("firstName", "testFirstName")
                         .param("lastName", "testLastName")
                         .param("DoB", "1980-01-01")
                         .param("email", "testEmail@gmail.com")
                         .param("isLastNameOptional", "false")
                         .with(csrf())
                 )
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("firstNameValid",""))
-                .andExpect(model().attribute("lastNameValid",""))
-                .andExpect(model().attribute("DoBValid",""));
-//                .andExpect(model().attribute("emailValid",""));
+                .andExpect(status().isOk());
+//                .andExpect(model().attribute("firstNameValid",""))
+//                .andExpect(model().attribute("lastNameValid",""))
+//                .andExpect(model().attribute("DoBValid",""));
+//                .andExpect(model().attrrmService = Mockito.mock(GardenerFormService.class);
+//        Mockito.whibute("emailValid",""));
 
-//
-//        ArgumentCaptor<Gardener> captor = ArgumentCaptor.forClass(Gardener.class);
-//        Mockito.verify(gardenerFormService).addGardener(captor.capture());
-//        Gardener updatedGardener = captor.getValue();
+//        gardenerCaptor = ArgumentCaptor.forClass(Gardener.class);
+//        Mockito.verify(gardenerFormService).addGardener(gardenerCaptor.capture());
+//        Gardener updatedGardener = gardenerCaptor.getValue();
 //        assertEquals("newTestFirstName", updatedGardener.getFirstName());
     }
 
-
-//    @Test
-//    @WithMockUser
-//    void onUserProfilePage_CorrectPasswordAndValidMatchingNewPasswordGiven_ChangePassword() throws Exception {
-//        UserProfileController userProfileController = new UserProfileController(gardenerFormService);
-//        MockMvc MOCK_MVC = MockMvcBuilders.standaloneSetup(userProfileController).build();
-//        MOCK_MVC.perform(post("/password")
-//                        .with(csrf())
-//                        .param("oldPassword", "Password1!")
-//                        .param("newPassword", "newPassword1@")
-//                        .param("retypePassword", "newPassword1@")
-//                )
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("redirect:/user"));
-//    }
-
-
+    @Test
+    @WithMockUser("testEmail@gmail.com")
+    void OnPasswordUpdatePage_userPutWrongOldPassword_errorMessageProvided() throws Exception {
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post("/password")
+                        .param("oldPassword", "wrongPassword")
+                        .param("newPassword", "NewPassWord1@")
+                        .param("retypePassword", "NewPassWord1@")
+                        .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(view().name("password"))
+                .andExpect(model().attribute("passwordCorrect", "Your old password is incorrect."));
+    }
+    @Test
+    @WithMockUser("testEmail@gmail.com")
+    void OnPasswordUpdatePage_userPutNotMatchedNewPasswords_errorMessageProvided() throws Exception {
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post("/password")
+                        .param("oldPassword", "testPassword")
+                        .param("newPassword", "NewPassWord1@")
+                        .param("retypePassword", "DifferentPassWord1@")
+                        .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(view().name("password"))
+                .andExpect(model().attribute("passwordsMatch", "Passwords do not match."));
+    }
+    @Test
+    @WithMockUser("testEmail@gmail.com")
+    void OnPasswordUpdatePage_userPutInvalidNewPasswords_errorMessageProvided() throws Exception {
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post("/password")
+                        .param("oldPassword", "testPassword")
+                        .param("newPassword", "newpassWord")
+                        .param("retypePassword", "newpassWord")
+                        .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(view().name("password"))
+                .andExpect(model().attribute("passwordStrong", "Your password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character."));
+    }
 }
