a1c0d08cfb17a5e3998528b208db8fc8bb35d477
Rinlada <rto52@uclive.ac.nz>
2024-05-01 11:25:43 +1200

    Wrote some tests for the login controller, Need to fix the test for authenticated users

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/LoginControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/LoginControllerTest.java
@@ -1,22 +1,50 @@
 package nz.ac.canterbury.seng302.gardenersgrove.integration.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import nz.ac.canterbury.seng302.gardenersgrove.controller.LoginController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LoginController.class)
 public class LoginControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private HttpServletResponse response;
+    @MockBean
+    private HttpServletRequest request;
+    @MockBean
+    private AuthenticationManager authenticationManager;
+    @MockBean
+    private Authentication authentication;
+
 
     @Test
     @WithMockUser
@@ -33,5 +61,28 @@ public class LoginControllerTest {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/login"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void onLoginPage_notVerified_redirectsToSignUpCodeForm() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("SPRING_SECURITY_LAST_EXCEPTION", "Email not verified");
+        Mockito.when(request.getSession()).thenReturn(session);
+        LoginController loginController = new LoginController();
+        MockMvc MOCK_MVC = MockMvcBuilders.standaloneSetup(loginController).build();
+        MOCK_MVC.perform(MockMvcRequestBuilders.get("/login").session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/signup"));
+    }
+
+    @Test
+    @WithMockUser
+    void onLoginPage_authenticatedUser_redirectsToGardens() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("SPRING_SECURITY_LAST_EXCEPTION", null);
+        Mockito.when(request.getSession()).thenReturn(session);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/login").session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/gardens"));
     }
 }
