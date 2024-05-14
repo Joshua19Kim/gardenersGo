17071846a115dfc3e9b71edc23c4f2574cc60fa3
Isaac Steele <ist46@uclive.ac.nz>
2024-05-14 17:14:03 +1200

    Wrote unit tests for the RequestService class.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/service/RequestServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/service/RequestServiceTest.java
@@ -2,6 +2,7 @@ package nz.ac.canterbury.seng302.gardenersgrove.unit.service;
 
 import nz.ac.canterbury.seng302.gardenersgrove.controller.GardenFormController;
 import nz.ac.canterbury.seng302.gardenersgrove.service.RequestService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -22,7 +23,22 @@ public class RequestServiceTest {
         mockHttpServletRequest.setRequestURI("/test/gardens/details");
         mockHttpServletRequest.setQueryString("gardenId=1");
 
+        String actualRequestURI = requestService.getRequestURI(mockHttpServletRequest);
+        String expectedRequestURI = "/gardens/details?gardenId=1";
 
+        Assertions.assertEquals(expectedRequestURI, actualRequestURI);
+
+    }
+
+    @Test
+    public void RequestURIRequested_NoContextPathAndParameters_ReturnsRequestURI() throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.setRequestURI("/gardens");
+
+        String actualRequestURI = requestService.getRequestURI(mockHttpServletRequest);
+        String expectedRequestURI = "/gardens";
+
+        Assertions.assertEquals(expectedRequestURI, actualRequestURI);
 
     }
 }
