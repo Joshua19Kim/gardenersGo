3b43e5498ccb296abcc12e3e0c1d2bed71811c7e
Isaac Steele <ist46@uclive.ac.nz>
2024-05-15 18:07:42 +1200

    Implemented caching for the weather API. - Added the spring-boot-starter-cache dependency to the build.gradle file - Cached the value returned by the getCurrentWeather method - Implemented a scheduler to clear the cache every hour - Tested that using caching was faster

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
@@ -30,6 +33,7 @@ public class WeatherService {
         WeatherService.api_key = api_key;
     }
 
+    @Cacheable(value = "currentWeather", key="#location")
     public HashMap<String, LinkedTreeMap<String, Object>> getCurrentWeather(String location) throws IOException, URISyntaxException {
         String uri = CURRENT_WEATHER_URL + "?key=" + api_key + "&q=" + location + "&aqi=no";
         URL url = new URI(uri).toURL();
@@ -46,6 +50,12 @@ public class WeatherService {
         } finally {
             connection.disconnect();
         }
+    }
+
+    @CacheEvict(value = "currentWeather", allEntries = true)
+    @Scheduled(fixedRateString = "${caching.spring.currentWeatherTTL}")
+    public void emptyCurrentWeatherCache() {
+        logger.info("Emptying current weather cache");
     }
 
 
