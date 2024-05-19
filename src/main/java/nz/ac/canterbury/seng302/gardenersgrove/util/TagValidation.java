c68d32fe3130fabbf02130c7417e31b7036621ab
Rinlada Tolley (rto52) <rto52@uclive.ac.nz>
2024-05-20 01:10:57 +1200

    Wrote more integration tests for add tags. Checked that invalid cases are redirected appropriately.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/util/TagValidation.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/util/TagValidation.java
@@ -4,23 +4,40 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.TagService;
 
 import java.util.Optional;
 
+/** Utility class for validating and checking the usage of tags. */
 public class TagValidation {
-    private final TagService tagService;
+  private final TagService tagService;
 
-    public TagValidation(TagService tagService) {
-        this.tagService = tagService;
-    }
+  public TagValidation(TagService tagService) {
+    this.tagService = tagService;
+  }
 
-    public Optional<String> validateTag(String tagName) {
-        String tagRegex = "^(?=.*[a-zA-Z0-9])[a-zA-Z0-9 _\\-'\"]+$";
-        if (tagName.length() >25) {
-            return Optional.of("A tag cannot exceed 25 characters");
-        } else {
-            return tagName.matches(tagRegex) ? Optional.empty() : Optional.of("The tag name must only contain alphanumeric characters, spaces, -, _, ', or \"");
-        }
+  /**
+   * Validates a tag based on its length and character
+   *
+   * @param tagName the tag name to validate
+   * @return an error message if validation fails, or empty if the tag name
+   *     is valid
+   */
+  public Optional<String> validateTag(String tagName) {
+    String tagRegex = "^(?=.*[a-zA-Z0-9])[a-zA-Z0-9 _\\-'\"]+$";
+    if (tagName.length() > 25) {
+      return Optional.of("A tag cannot exceed 25 characters");
+    } else {
+      return tagName.matches(tagRegex)
+          ? Optional.empty()
+          : Optional.of(
+              "The tag name must only contain alphanumeric characters, spaces, -, _, ', or \"");
     }
+  }
 
-    public Optional<String> checkTagInUse(String tagName) {
-        return (tagService.findTagByName(tagName).isPresent() ? Optional.of("Used") : Optional.empty());
-    }
+    /**
+     * checks if a tag with the given name is already in use, to prevent duplicates
+     *
+     * @param tagName the tag name to check
+     * @return "Used" if the tag is in use, or empty if the tag is not found.
+     */
+  public Optional<String> checkTagInUse(String tagName) {
+    return (tagService.findTagByName(tagName).isPresent() ? Optional.of("Used") : Optional.empty());
+  }
 }
