80602940f02576280d20f584cbf95881f4b6eb8d
Rinlada Tolley (rto52) <rto52@uclive.ac.nz>
2024-05-27 23:55:48 +1200

    Added column to Garden entity to track the last time the weather notification is closed.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/GardenFormControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/GardenFormControllerTest.java
@@ -792,7 +792,7 @@ public class GardenFormControllerTest {
                 .andExpect(model().attribute("forecastTemperature",List.of(forecastTemperatures)))
                 .andExpect(model().attribute("forecastWeatherImage",List.of(forecastImages)))
                 .andExpect(model().attribute("forecastWeatherDescription",List.of(forecastDescriptions)))
-                .andExpect(model().attribute("forcastHumidities",List.of(forecastHumidities)))
+                .andExpect(model().attribute("forecastHumidities",List.of(forecastHumidities)))
                 .andExpect(model().attribute("garden", garden));
     }
 
@@ -820,7 +820,7 @@ public class GardenFormControllerTest {
                 .andExpect(model().attributeDoesNotExist("forecastTemperature"))
                 .andExpect(model().attributeDoesNotExist("forecastWeatherImage"))
                 .andExpect(model().attributeDoesNotExist("forecastWeatherDescription"))
-                .andExpect(model().attributeDoesNotExist("forcastHumidities"))
+                .andExpect(model().attributeDoesNotExist("forecastHumidities"))
                 .andExpect(model().attribute("garden", garden));
 
     }
