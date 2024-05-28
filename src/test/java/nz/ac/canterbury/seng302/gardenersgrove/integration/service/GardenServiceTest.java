d209c667c76a448047710b128d86e60cc8678110
Kush Desai <kde54@uclive.ac.nz>
2024-05-28 20:46:40 +1200

    Fixed tests by creating a new application.properties in the test folder so that all tests use an in memory h2 database

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/service/GardenServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/service/GardenServiceTest.java
@@ -1,117 +1,117 @@
-//package nz.ac.canterbury.seng302.gardenersgrove.integration.service;
-//
-//import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
-//import nz.ac.canterbury.seng302.gardenersgrove.entity.Gardener;
-//import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
-//import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@DataJpaTest
-//@Import(GardenService.class)
-//public class GardenServiceTest {
-//    Gardener testGardener = new Gardener("Test", "Gardener",
-//            LocalDate.of(2024, 4, 1), "testgardener@gmail.com",
-//            "Password1!");
-//
-//    @Test
-//    public void GardenAdded_ValidInputs_GardenSavedToRepository() {
-//        GardenService gardenService = new GardenService(new GardenRepository() {
-//            @Override
-//            public Optional<Garden> findById(long id) {
-//                return Optional.empty();
-//            }
-//
-//            @Override
-//            public List<Garden> findAll() {
-//                return null;
-//            }
-//
-//            @Override
-//            public List<Garden> findByGardenerId(Long gardenerId) {
-//                return List.of();
-//            }
-//
-//            @Override
-//            public <S extends Garden> S save(S entity) {
-//                Assertions.assertEquals(entity.getName(), "Botanical");
-//                Assertions.assertEquals(entity.getLocation(), "Homestead Lane");
-//                Assertions.assertEquals(entity.getSize(), "100");
-//                return entity;
-//            }
-//
-//            @Override
-//            public <S extends Garden> Iterable<S> saveAll(Iterable<S> entities) {
-//                return null;
-//            }
-//
-//            @Override
-//            public Optional<Garden> findById(Long aLong) {
-//                return Optional.empty();
-//            }
-//
-//            @Override
-//            public boolean existsById(Long aLong) {
-//                return false;
-//            }
-//
-//            @Override
-//            public Iterable<Garden> findAllById(Iterable<Long> longs) {
-//                return null;
-//            }
-//
-//            @Override
-//            public long count() {
-//                return 0;
-//            }
-//
-//            @Override
-//            public void deleteById(Long aLong) {
-//
-//            }
-//
-//            @Override
-//            public void delete(Garden entity) {
-//
-//            }
-//
-//            @Override
-//            public void deleteAllById(Iterable<? extends Long> longs) {
-//
-//            }
-//
-//            @Override
-//            public void deleteAll(Iterable<? extends Garden> entities) {
-//
-//            }
-//
-//            @Override
-//            public void deleteAll() {
-//
-//            }
-//        });
-//        gardenService.addGarden(new Garden("Botanical", "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener)
-//        );
-//    }
-//
-//    @Autowired
-//    private GardenRepository gardenRepository;
-//
-//    @Test
-//    public void GardenAdded_ValidInputs_GardenReturned() {
-//        GardenService gardenService = new GardenService(gardenRepository);
-//        Garden garden = gardenService.addGarden(new Garden("Botanical", "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener));
-//        Assertions.assertEquals(garden.getName(), "Botanical");
-//        Assertions.assertEquals(garden.getLocation(), "Homestead Lane");
-//        Assertions.assertEquals(garden.getSize(), "100");
-//    }
-//
-//
-//}
+package nz.ac.canterbury.seng302.gardenersgrove.integration.service;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Gardener;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@Import(GardenService.class)
+public class GardenServiceTest {
+    Gardener testGardener = new Gardener("Test", "Gardener",
+            LocalDate.of(2024, 4, 1), "testgardener@gmail.com",
+            "Password1!");
+
+    @Test
+    public void GardenAdded_ValidInputs_GardenSavedToRepository() {
+        GardenService gardenService = new GardenService(new GardenRepository() {
+            @Override
+            public Optional<Garden> findById(long id) {
+                return Optional.empty();
+            }
+
+            @Override
+            public List<Garden> findAll() {
+                return null;
+            }
+
+            @Override
+            public List<Garden> findByGardenerId(Long gardenerId) {
+                return List.of();
+            }
+
+            @Override
+            public <S extends Garden> S save(S entity) {
+                Assertions.assertEquals(entity.getName(), "Botanical");
+                Assertions.assertEquals(entity.getLocation(), "Homestead Lane");
+                Assertions.assertEquals(entity.getSize(), "100");
+                return entity;
+            }
+
+            @Override
+            public <S extends Garden> Iterable<S> saveAll(Iterable<S> entities) {
+                return null;
+            }
+
+            @Override
+            public Optional<Garden> findById(Long aLong) {
+                return Optional.empty();
+            }
+
+            @Override
+            public boolean existsById(Long aLong) {
+                return false;
+            }
+
+            @Override
+            public Iterable<Garden> findAllById(Iterable<Long> longs) {
+                return null;
+            }
+
+            @Override
+            public long count() {
+                return 0;
+            }
+
+            @Override
+            public void deleteById(Long aLong) {
+
+            }
+
+            @Override
+            public void delete(Garden entity) {
+
+            }
+
+            @Override
+            public void deleteAllById(Iterable<? extends Long> longs) {
+
+            }
+
+            @Override
+            public void deleteAll(Iterable<? extends Garden> entities) {
+
+            }
+
+            @Override
+            public void deleteAll() {
+
+            }
+        });
+        gardenService.addGarden(new Garden("Botanical", "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener)
+        );
+    }
+
+    @Autowired
+    private GardenRepository gardenRepository;
+
+    @Test
+    public void GardenAdded_ValidInputs_GardenReturned() {
+        GardenService gardenService = new GardenService(gardenRepository);
+        Garden garden = gardenService.addGarden(new Garden("Botanical", "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener));
+        Assertions.assertEquals(garden.getName(), "Botanical");
+        Assertions.assertEquals(garden.getLocation(), "Homestead Lane");
+        Assertions.assertEquals(garden.getSize(), "100");
+    }
+
+
+}
--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/service/PlantServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/service/PlantServiceTest.java
@@ -1,118 +1,118 @@
-//package nz.ac.canterbury.seng302.gardenersgrove.integration.service;
-//
-//import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
-//import nz.ac.canterbury.seng302.gardenersgrove.entity.Gardener;
-//import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
-//import nz.ac.canterbury.seng302.gardenersgrove.repository.PlantRepository;
-//import nz.ac.canterbury.seng302.gardenersgrove.service.PlantService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@DataJpaTest
-//@Import(PlantService.class)
-//public class PlantServiceTest {
-//    Gardener testGardener = new Gardener("Test", "Gardener",
-//            LocalDate.of(2024, 4, 1), "testgardener@gmail.com",
-//            "Password1!");
-//
-//    @Test
-//    public void PlantAdded_ValidInputs_PlantSavedToRepository() {
-//        Garden garden = new Garden("Botanical","20 Marquess street",null,"ChristChurch","New Zealand", "8870", testGardener);
-//        PlantService plantService = new PlantService(new PlantRepository() {
-//            @Override
-//            public Optional<Plant> findById(long id) {
-//                return Optional.empty();
-//            }
-//
-//            @Override
-//            public List<Plant> findAll() {
-//                return null;
-//            }
-//
-//            @Override
-//            public <S extends Plant> S save(S entity) {
-//                Assertions.assertEquals(entity.getName(), "Flower");
-//                Assertions.assertEquals(entity.getCount(), "2");
-//                Assertions.assertEquals(entity.getDescription(), "Rose");
-//                Assertions.assertEquals(entity.getDatePlanted(), "08/02/2024");
-//                Assertions.assertEquals(entity.getGarden(), garden);
-//                return entity;
-//            }
-//
-//            @Override
-//            public <S extends Plant> Iterable<S> saveAll(Iterable<S> entities) {
-//                return null;
-//            }
-//
-//            @Override
-//            public Optional<Plant> findById(Long aLong) {
-//                return Optional.empty();
-//            }
-//
-//            @Override
-//            public boolean existsById(Long aLong) {
-//                return false;
-//            }
-//
-//            @Override
-//            public Iterable<Plant> findAllById(Iterable<Long> longs) {
-//                return null;
-//            }
-//
-//            @Override
-//            public long count() {
-//                return 0;
-//            }
-//
-//            @Override
-//            public void deleteById(Long aLong) {
-//
-//            }
-//
-//            @Override
-//            public void delete(Plant entity) {
-//
-//            }
-//
-//            @Override
-//            public void deleteAllById(Iterable<? extends Long> longs) {
-//
-//            }
-//
-//            @Override
-//            public void deleteAll(Iterable<? extends Plant> entities) {
-//
-//            }
-//
-//            @Override
-//            public void deleteAll() {
-//
-//            }
-//        });
-//        plantService.addPlant(new Plant("Flower","2", "Rose", "08/02/2024", garden));
-//    }
-//
-//    @Autowired
-//    private PlantRepository plantRepository;
-//
-//    @Test
-//    public void PlantAdded_ValidInputs_PlantReturned() {
-//        PlantService plantService = new PlantService(plantRepository);
-//        Garden garden = new Garden("Botanical","20 Marquess street",null,"ChristChurch","New Zealand", "8870", testGardener);
-//        Plant plant = plantService.addPlant(new Plant("Flower","2", "Rose", "08/02/2024", garden));
-//        Assertions.assertEquals(plant.getName(), "Flower");
-//        Assertions.assertEquals(plant.getCount(), "2");
-//        Assertions.assertEquals(plant.getDescription(), "Rose");
-//        Assertions.assertEquals(plant.getDatePlanted(), "08/02/2024");
-//        Assertions.assertEquals(plant.getGarden(), garden);
-//    }
-//
-//
-//}
+package nz.ac.canterbury.seng302.gardenersgrove.integration.service;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Gardener;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.PlantRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.service.PlantService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@Import(PlantService.class)
+public class PlantServiceTest {
+    Gardener testGardener = new Gardener("Test", "Gardener",
+            LocalDate.of(2024, 4, 1), "testgardener@gmail.com",
+            "Password1!");
+
+    @Test
+    public void PlantAdded_ValidInputs_PlantSavedToRepository() {
+        Garden garden = new Garden("Botanical","20 Marquess street",null,"ChristChurch","New Zealand", "8870", testGardener);
+        PlantService plantService = new PlantService(new PlantRepository() {
+            @Override
+            public Optional<Plant> findById(long id) {
+                return Optional.empty();
+            }
+
+            @Override
+            public List<Plant> findAll() {
+                return null;
+            }
+
+            @Override
+            public <S extends Plant> S save(S entity) {
+                Assertions.assertEquals(entity.getName(), "Flower");
+                Assertions.assertEquals(entity.getCount(), "2");
+                Assertions.assertEquals(entity.getDescription(), "Rose");
+                Assertions.assertEquals(entity.getDatePlanted(), "08/02/2024");
+                Assertions.assertEquals(entity.getGarden(), garden);
+                return entity;
+            }
+
+            @Override
+            public <S extends Plant> Iterable<S> saveAll(Iterable<S> entities) {
+                return null;
+            }
+
+            @Override
+            public Optional<Plant> findById(Long aLong) {
+                return Optional.empty();
+            }
+
+            @Override
+            public boolean existsById(Long aLong) {
+                return false;
+            }
+
+            @Override
+            public Iterable<Plant> findAllById(Iterable<Long> longs) {
+                return null;
+            }
+
+            @Override
+            public long count() {
+                return 0;
+            }
+
+            @Override
+            public void deleteById(Long aLong) {
+
+            }
+
+            @Override
+            public void delete(Plant entity) {
+
+            }
+
+            @Override
+            public void deleteAllById(Iterable<? extends Long> longs) {
+
+            }
+
+            @Override
+            public void deleteAll(Iterable<? extends Plant> entities) {
+
+            }
+
+            @Override
+            public void deleteAll() {
+
+            }
+        });
+        plantService.addPlant(new Plant("Flower","2", "Rose", "08/02/2024", garden));
+    }
+
+    @Autowired
+    private PlantRepository plantRepository;
+
+    @Test
+    public void PlantAdded_ValidInputs_PlantReturned() {
+        PlantService plantService = new PlantService(plantRepository);
+        Garden garden = new Garden("Botanical","20 Marquess street",null,"ChristChurch","New Zealand", "8870", testGardener);
+        Plant plant = plantService.addPlant(new Plant("Flower","2", "Rose", "08/02/2024", garden));
+        Assertions.assertEquals(plant.getName(), "Flower");
+        Assertions.assertEquals(plant.getCount(), "2");
+        Assertions.assertEquals(plant.getDescription(), "Rose");
+        Assertions.assertEquals(plant.getDatePlanted(), "08/02/2024");
+        Assertions.assertEquals(plant.getGarden(), garden);
+    }
+
+
+}
