5e7b53ba1b59c4dfb2f7878610aa1e6151279f61
Arthur Bell <abe118@uclive.ac.nz>
2024-05-29 01:01:53 +1200

    Make more progress on cucumber tests

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/step_definitions/ResetPasswordFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/step_definitions/ResetPasswordFeature.java
@@ -1,24 +1,49 @@
 package nz.ac.canterbury.seng302.gardenersgrove.step_definitions;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import nz.ac.canterbury.seng302.gardenersgrove.controller.LoginController;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class ResetPasswordFeature {
+
+    private MockMvc mockMvc;
+    private MvcResult mvcResult;
+
+    @Before
+    public void setUp() {
+        LoginController loginController = new LoginController();
+        mockMvc = MockMvcBuilders.standaloneSetup(loginController).build();
+    }
+
     @Given("I am on the login page")
-    public void i_am_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_am_on_the_login_page() throws Exception {
+        mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(status().isOk())
+        .andReturn();
     }
+
     @When("I hit the {string} link")
-    public void i_hit_the_link(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_hit_the_link(String linkName) {
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+
     }
-    @Then("I see a form asking me for my email address")
-    public void i_see_a_form_asking_me_for_my_email_address() {
+
+    @Then("I am taken to the {string} page")
+    public void i_am_taken_to_the_page(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
--- src/test/resources/U16-ResetPassword.feature
+++ src/test/resources/U16-ResetPassword.feature
@@ -3,4 +3,4 @@ Feature: U16 - As Sarah, I want to be able to change my password over email, so 
 Scenario: AC1
   Given I am on the login page
   When I hit the "Forgot your password?" link
-  Then I see a form asking me for my email address
+  Then I am taken to the "forgotPasswordForm" page
