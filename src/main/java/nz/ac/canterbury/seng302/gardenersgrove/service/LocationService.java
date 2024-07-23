6b970da809fd64973f22146d8f9ee8aefcdd8700
Benjamin Moore <bmo80@uclive.ac.nz>
2024-07-23 18:04:21 +1200

    Fixed unit test - needed to add Config Class to set up a new bean for HttpClient. Will work on integration and cucumber tests next.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/LocationService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/LocationService.java
@@ -20,13 +20,16 @@ import java.net.http.HttpResponse;
 public class LocationService {
     Logger logger = LoggerFactory.getLogger(LocationService.class);
     private String api_key;
+    private HttpClient client;
 
     /**
      * Constructor of LocationService.
      * @param api_key The credential key for the Location API. It is saved as an environment variable.
      */
     @Autowired
-    public LocationService(@Value("${locationIq.password}") String api_key) {this.api_key = api_key;
+    public LocationService(@Value("${locationIq.password}") String api_key, HttpClient client) {
+        this.api_key = api_key;
+        this.client = client;
     }
 
     /**
@@ -49,7 +52,7 @@ public class LocationService {
 
 
         // Needs to be HttpClient type as it goes back to HTML.
-        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return this.client.send(request, HttpResponse.BodyHandlers.ofString());
 
     }
 }
