2fadd383ec18e7995a6efbed696e8723d47b7d37
Sam Dawson <sda110@uclive.ac.nz>
2024-08-02 19:06:44 +1200

    Added fixes to applicaiton properties and environment variables in order to get cucumber tests running, wrote cucumber tests, fixed bugs etc

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/RunCucumberTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/RunCucumberTest.java
@@ -4,8 +4,14 @@ package nz.ac.canterbury.seng302.gardenersgrove.cucumber;
 import io.cucumber.junit.platform.engine.Constants;
 import io.cucumber.spring.CucumberContextConfiguration;
 import nz.ac.canterbury.seng302.gardenersgrove.GardenersGroveApplication;
+import nz.ac.canterbury.seng302.gardenersgrove.service.EmailUserService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.LocationService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.WeatherService;
 import org.junit.platform.suite.api.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -20,6 +26,11 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(classes = GardenersGroveApplication.class)
 @CucumberContextConfiguration
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("cucumber")
+@MockBean(WeatherService.class)
+@MockBean(Authentication.class)
+@MockBean(LocationService.class)
+@MockBean(EmailUserService.class)
 public class RunCucumberTest {
 }
