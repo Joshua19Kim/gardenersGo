88ed109739b10d0358a65aba0e82765277ad01a4
Joshua Kim <bki42@uclive.ac.nz>
2024-08-03 23:14:52 +1200

    Added some more integration tests for GardenVisit. Added DELETE sql query for gardenVisit to clean up records during test.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/GardenVisitRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/GardenVisitRepository.java
@@ -3,15 +3,35 @@ package nz.ac.canterbury.seng302.gardenersgrove.repository;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenVisit;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface GardenVisitRepository extends CrudRepository<GardenVisit, Long> {
+
+    /**
+     * Find the most recent gardens that the certain gardener visited
+     * @param gardenerId The identifier of the garden's owner.
+     * @param pageable Pagination information to limit and order the results.
+     * @return A list of Garden, representing the most recently visited gardens by the specified gardener,
+     *         ordered by the most recent visit time in descending order.
+     */
     @Query("SELECT gv.garden FROM GardenVisit gv WHERE gv.gardener.id = :gardenerId GROUP BY gv.garden ORDER BY MAX(gv.accessTime) DESC")
     List<Garden> findRecentGardensByGardenerId(@Param("gardenerId") Long gardenerId, Pageable pageable);
+
+    /**
+     * Delete the garden visit records of the specific gardner with ID
+     * @param gardenerId The identifier of the garden's owner.
+     */
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM garden_visit WHERE gardener_id = :gardenerId", nativeQuery = true)
+    void deleteAllGardenVisitsByGardenerId(@Param("gardenerId") Long gardenerId);
+
 }
--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/service/GardenVisitServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/service/GardenVisitServiceTest.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Import;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @DataJpaTest
 @Import(GardenVisitService.class)
@@ -33,38 +34,112 @@ public class GardenVisitServiceTest {
     @Autowired
     private GardenVisitRepository gardenVisitRepository;
     private GardenVisitService gardenVisitService;
+    private GardenService gardenService;
     private Gardener testGardener;
-    private Garden testGarden;
 
     @BeforeEach
     public void setUp() {
-        GardenService gardenService = new GardenService(gardenRepository);
+        gardenService = new GardenService(gardenRepository);
         GardenerFormService gardenerFormService = new GardenerFormService(gardenerFormRepository, lostPasswordTokenRepository);
         gardenVisitService = new GardenVisitService(gardenVisitRepository);
 
         testGardener = new Gardener("Test", "Gardener",
                 LocalDate.of(2024, 4, 1), "testgardener@gmail.com",
                 "Password1!");
+        testGardener.setId(1L);
         gardenerFormService.addGardener(testGardener);
 
-
-        testGarden = new Garden("Botanical",
-                "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener, "");
-        gardenService.addGarden(testGarden);
     }
 
     @AfterEach
     public void tearDown() {
+        gardenVisitRepository.deleteAllGardenVisitsByGardenerId(testGardener.getId());
         gardenRepository.deleteAll();
         gardenerFormRepository.deleteAll();
     }
 
     @Test
-    public void GardenVisitAdded_ValidInputs_GardenVisitReturned() {
+    public void GardenVisitAdded_OneGarden_GardenVisitReturnedAndShowGardenInRecentList() {
+        Garden testBotanicalGarden = new Garden("Botanical",
+                "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener, "");
+        gardenService.addGarden(testBotanicalGarden);
+
         LocalDateTime currentTime = LocalDateTime.now();
-        GardenVisit gardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testGarden, currentTime));
+        GardenVisit gardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testBotanicalGarden, currentTime));
+
         Assertions.assertEquals(gardenVisit.getGardener(), testGardener);
-        Assertions.assertEquals(gardenVisit.getGarden(), testGarden);
+        Assertions.assertEquals(gardenVisit.getGarden(), testBotanicalGarden);
         Assertions.assertEquals(gardenVisit.getAccessTime(), currentTime);
+
+        List<Garden> recentGardens = gardenVisitService.findRecentGardensByGardenerId(testGardener.getId());
+
+        Assertions.assertTrue(recentGardens.contains(testBotanicalGarden));
     }
+
+    @Test
+    public void GardenVisitAdded_TwoDifferentGardens_GardenVisitReturnedAndShowGardensInRecentList() {
+        Garden testBotanicalGarden = new Garden("Botanical",
+                "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener, "");
+        gardenService.addGarden(testBotanicalGarden);
+        Garden testRoseGarden = new Garden("Rose Garden",
+                "22 Kirkwood street", null, "Christchurch", "New Zealand", null, "25", testGardener, "");
+        gardenService.addGarden(testRoseGarden);
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        GardenVisit botanicalGardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testBotanicalGarden, currentTime));
+        GardenVisit roseGardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testRoseGarden, currentTime));
+
+        Assertions.assertEquals(botanicalGardenVisit.getGardener(), testGardener);
+        Assertions.assertEquals(botanicalGardenVisit.getGarden(), testBotanicalGarden);
+        Assertions.assertEquals(botanicalGardenVisit.getAccessTime(), currentTime);
+
+        Assertions.assertEquals(roseGardenVisit.getGardener(), testGardener);
+        Assertions.assertEquals(roseGardenVisit.getGarden(), testRoseGarden);
+        Assertions.assertEquals(roseGardenVisit.getAccessTime(), currentTime);
+
+        List<Garden> recentGardens = gardenVisitService.findRecentGardensByGardenerId(testGardener.getId());
+        Assertions.assertTrue(recentGardens.contains(testBotanicalGarden));
+        Assertions.assertTrue(recentGardens.contains(testRoseGarden));
+    }
+
+    @Test
+    public void GardenVisitAdded_ThreeDifferentGardens_GardenVisitReturnedAndShowGardensInRecentList() {
+        Garden testBotanicalGarden = new Garden("Botanical",
+                "Homestead Lane", null, "Christchurch", "New Zealand", null, "100", testGardener, "");
+        gardenService.addGarden(testBotanicalGarden);
+        Garden testRoseGarden = new Garden("Rose Garden",
+                "22 Kirkwood street", null, "Christchurch", "New Zealand", null, "25", testGardener, "");
+        gardenService.addGarden(testRoseGarden);
+        Garden testTulipGarden = new Garden("Tulip Garden",
+                "100 Lovely street", null, "Busan", "South Korea", null, "3000", testGardener, "");
+        gardenService.addGarden(testTulipGarden);
+
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        GardenVisit botanicalGardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testBotanicalGarden, currentTime));
+        GardenVisit roseGardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testRoseGarden, currentTime));
+        GardenVisit tulipGardenVisit = gardenVisitService.addGardenVisit(new GardenVisit(testGardener, testTulipGarden, currentTime));
+
+
+        Assertions.assertEquals(botanicalGardenVisit.getGardener(), testGardener);
+        Assertions.assertEquals(botanicalGardenVisit.getGarden(), testBotanicalGarden);
+        Assertions.assertEquals(botanicalGardenVisit.getAccessTime(), currentTime);
+
+        Assertions.assertEquals(roseGardenVisit.getGardener(), testGardener);
+        Assertions.assertEquals(roseGardenVisit.getGarden(), testRoseGarden);
+        Assertions.assertEquals(roseGardenVisit.getAccessTime(), currentTime);
+
+        Assertions.assertEquals(tulipGardenVisit.getGardener(), testGardener);
+        Assertions.assertEquals(tulipGardenVisit.getGarden(), testTulipGarden);
+        Assertions.assertEquals(tulipGardenVisit.getAccessTime(), currentTime);
+
+        List<Garden> recentGardens = gardenVisitService.findRecentGardensByGardenerId(testGardener.getId());
+        Assertions.assertTrue(recentGardens.contains(testBotanicalGarden));
+        Assertions.assertTrue(recentGardens.contains(testRoseGarden));
+        Assertions.assertTrue(recentGardens.contains(testTulipGarden));
+
+    }
+
+
+
 }
