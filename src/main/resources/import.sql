4f53dcdb00799bfda930efbd90fae58494ab5752
Joshua Kim <bki42@uclive.ac.nz>
2024-08-16 16:22:26 +1200

    tided up sql query for local

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -55,49 +55,53 @@ INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Urban Jungle', '111 Concrete Jungle St', 'Metro', 'New York', 'USA', '10001', '25', true, '2019-01-30');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Vertical Garden', '888 Wall St', 'Financial District', 'Singapore', 'Singapore', '018989', '20', true, '2021-06-30');
 
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Fairy Garden', '1515 Whimsical Way', 'Enchanted Forest', 'Dublin', 'Ireland', 'D01 F5P2', '35', true, '2023-10-31');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Rooftop Haven', '789 Skyline Ave', 'Downtown', 'Chicago', 'USA', '60601', '30', true, '2017-05-10');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Captain''s Victory Garden', '569 Leaman Place', 'Brooklyn Heights', 'New York', 'USA', '11201', '100', true, '2015-07-04');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Black Widow''s Secret Garden', '1 Red Square', null, 'Moscow', 'Russia', '109012', '300', true, '2015-11-22');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Asgardian Courtyard', '1 Rainbow Bridge', null, 'Asgard', 'Nine Realms', null, '10', true, '2016-06-15');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Emerald Sanctuary', '123 Green St', 'Riverside', 'Portland', 'USA', '97201', '50', true, '2016-10-01');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Zen Retreat', '45 Peaceful Ln', 'Harmony', 'Kyoto', 'Japan', '600-8216', '75', true, '2017-01-20');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Wildflower Meadow', '234 Prairie Rd', 'Grasslands', 'Austin', 'USA', '78701', '200', true, '2017-09-05');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Tropical Paradise', '56 Palm Beach Dr', 'Seaside', 'Miami', 'USA', '33101', '150', true, '2018-01-12');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Bamboo Forest', '444 Panda Path', 'Eastside', 'Chengdu', 'China', '610000', '300', true, '2020-02-11');
+
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Rock Garden', '1111 Boulder Blvd', 'Stonehenge', 'Salisbury', 'UK', 'SP4 7DE', '180', true, '2022-07-21');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Permaculture Paradise', '3232 Sustainable St', 'Eco Village', 'Byron Bay', 'Australia', '2481', '400', true, '2019-06-21');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Hanging Gardens', '1818 Cascade Falls', 'Waterfall', 'Bablyon', 'Iraq', '51001', '400', true, '2024-05-07');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Asgardian Courtyard', '1 Rainbow Bridge', null, 'Asgard', 'Nine Realms', null, '10', true, '2016-06-15');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Black Widow''s Secret Garden', '1 Red Square', null, 'Moscow', 'Russia', '109012', '300', true, '2015-11-22');
+
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Zen Retreat', '45 Peaceful Ln', 'Harmony', 'Kyoto', 'Japan', '600-8216', '75', true, '2017-01-20');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (4, 'Alpine Retreat', '78 Mountain View Rd', 'Highlands', 'Zurich', 'Switzerland', '8001', '100', true, '2018-05-18');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (7, 'Desert Oasis', '910 Cactus Way', 'Sands', 'Phoenix', 'USA', '85001', '80', true, '2018-09-22');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Cottage Garden', '222 Quaint Lane', 'Countryside', 'Cotswolds', 'UK', 'GL54 2AZ', '60', true, '2019-06-14');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Emerald Sanctuary', '123 Green St', 'Riverside', 'Portland', 'USA', '97201', '50', true, '2016-10-01');
 
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Butterfly Haven', '333 Flutter By Way', 'Meadowlands', 'San Diego', 'USA', '92101', '40', true, '2019-10-05');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Bamboo Forest', '444 Panda Path', 'Eastside', 'Chengdu', 'China', '610000', '300', true, '2020-02-11');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Herb Paradise', '555 Aroma Ave', 'Flavortown', 'Provence', 'France', '13100', '45', true, '2020-06-20');
+INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Wildflower Meadow', '234 Prairie Rd', 'Grasslands', 'Austin', 'USA', '78701', '200', true, '2017-09-05');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (4, 'Succulent Sanctuary', '666 Dusty Rd', 'Dry Gulch', 'Tucson', 'USA', '85701', '70', true, '2020-10-25');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (7, 'Rose Garden', '777 Thorny Path', 'Fragrant Hills', 'Portland', 'USA', '97205', '90', true, '2021-02-14');
+
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Meditation Garden', '999 Calm Court', 'Serenity', 'Bali', 'Indonesia', '80361', '110', true, '2021-11-22');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Fruit Orchard', '1010 Juicy Lane', 'Tasty Town', 'Valencia', 'Spain', '46001', '250', true, '2022-03-18');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Rock Garden', '1111 Boulder Blvd', 'Stonehenge', 'Salisbury', 'UK', 'SP4 7DE', '180', true, '2022-07-21');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Rain Forest', '1212 Mist Ave', 'Cloudforest', 'Manaus', 'Brazil', '69000-000', '500', true, '2022-11-15');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (4, 'Seaside Garden', '1313 Ocean View Dr', 'Coastal', 'Sydney', 'Australia', '2000', '85', true, '2023-03-08');
-
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (7, 'Bonsai Collection', '1414 Miniature Rd', 'Tiny Town', 'Tokyo', 'Japan', '100-0005', '15', true, '2023-07-09');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Fairy Garden', '1515 Whimsical Way', 'Enchanted Forest', 'Dublin', 'Ireland', 'D01 F5P2', '35', true, '2023-10-31');
+
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Cactus Canyon', '1616 Prickly Pear Rd', 'Desertscape', 'Sedona', 'USA', '86336', '120', true, '2024-02-15');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Cherry Blossom Park', '1717 Sakura St', 'Hanami', 'Kyoto', 'Japan', '605-0862', '200', true, '2024-04-01');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Hanging Gardens', '1818 Cascade Falls', 'Waterfall', 'Bablyon', 'Iraq', '51001', '400', true, '2024-05-07');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Lavender Fields', '1919 Purple Haze Ln', 'Fragrant Valley', 'Provence', 'France', '84400', '300', true, '2024-06-14');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (4, 'Sunflower Meadow', '2020 Sunny Side Up', 'Bright Valley', 'Tuscany', 'Italy', '50122', '250', true, '2024-07-21');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (7, 'Arctic Greenhouse', '2121 Polar Express', 'Frozen Fjord', 'Tromsø', 'Norway', '9008', '50', true, '2015-08-01');
+
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Zen Rock Garden', '2222 Meditation Mews', 'Inner Peace', 'Kyoto', 'Japan', '605-0862', '75', true, '2015-12-05');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Hummingbird Haven', '2323 Nectar Nook', 'Flutterby', 'Costa Rica', 'Costa Rica', '10101', '40', true, '2016-04-15');
-
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Tulip Fields', '2424 Rainbow Row', 'Colorful Plains', 'Amsterdam', 'Netherlands', '1012 JS', '350', true, '2016-08-22');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Moss Garden', '2525 Velvet Verde', 'Lush Grove', 'Seattle', 'USA', '98101', '60', true, '2016-12-30');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Topiary Wonderland', '2626 Scissorhands St', 'Shapely Shrubs', 'Versailles', 'France', '78000', '150', true, '2017-04-15');
+
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (4, 'Prehistoric Garden', '2727 Jurassic Park', 'Ancient Grove', 'Kauai', 'USA', '96746', '500', true, '2017-08-31');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (7, 'Butterfly Conservatory', '2828 Metamorphosis Ave', 'Winged Wonders', 'Key West', 'USA', '33040', '80', true, '2018-01-11');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (5, 'Carnivorous Plant Bog', '3030 Venus Flytrap Rd', 'Hungry Hollow', 'North Carolina', 'USA', '28401', '30', true, '2018-09-01');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (3, 'Hydroponic Lab', '3131 Nutrient Flow', 'Futuristic Farms', 'Tokyo', 'Japan', '100-0005', '25', true, '2019-02-14');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (2, 'Permaculture Paradise', '3232 Sustainable St', 'Eco Village', 'Byron Bay', 'Australia', '2481', '400', true, '2019-06-21');
 INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (6, 'Rooftop Vineyard', '3333 Grape Expectations', 'Urban Winery', 'Napa', 'USA', '94559', '100', true, '2019-10-15');
-INSERT INTO garden (gardener_id, name, location, suburb, city, country, postcode, size, PUBLIC_GARDEN, creation_date) VALUES (1, 'Rooftop Haven', '789 Skyline Ave', 'Downtown', 'Chicago', 'USA', '60601', '30', true, '2017-05-10');
 
 
 INSERT INTO plant (name, garden_id, count, description, image) VALUES ('Lavender', 1, '2', 'Fragrant purple flowers', '/images/plantImageExample/lavender.png');
