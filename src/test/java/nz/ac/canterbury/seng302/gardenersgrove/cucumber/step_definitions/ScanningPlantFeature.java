fe65fcdc9632eda9ef867ee5512977d2f9c7bf11
Joshua Kim <bki42@uclive.ac.nz>
2024-08-29 14:19:54 +1200

    updated cucumber test

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/ScanningPlantFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/ScanningPlantFeature.java
@@ -11,6 +11,7 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.GardenerFormService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.ImageService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.PlantIdentificationService;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,6 +22,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.xml.transform.Result;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -35,76 +37,51 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 public class ScanningPlantFeature {
-    private MockMvc mockScanControllerMvc;
-    private Gardener testGardener;
-    private Authentication authentication;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
     private GardenerFormService gardenerFormService;
-    private ImageService imageService;
-    private PlantIdentificationService plantIdentificationService;
-    private MockMultipartFile genericImage;
-    private ResultActions result;
+    private MockMultipartFile imageFile;
+    private Gardener gardener;
+    private ResultActions resultActions;
 
     @Before("@U7001")
     public void setUp() {
-        testGardener = new Gardener("Test", "Gardener",
-                LocalDate.of(2024, 4, 1), "testgardener@gmail.com",
-                "Password1!");
-
-        authentication = Mockito.mock(Authentication.class);
-        Authentication auth = new UsernamePasswordAuthenticationToken("testgardener@gmail.com", "Password1!");
-        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-        securityContext.setAuthentication(auth);
-        SecurityContextHolder.setContext(securityContext);
-        when(authentication.getName()).thenReturn("testgardener@gmail.com");
-
-        plantIdentificationService = Mockito.mock(PlantIdentificationService.class);
-        imageService = Mockito.mock(ImageService.class);
-        gardenerFormService = Mockito.mock(GardenerFormService.class);
-        ScanController scanController = new ScanController(plantIdentificationService, gardenerFormService, imageService);
-
-        mockScanControllerMvc = MockMvcBuilders.standaloneSetup(scanController).build();
+        Optional<Gardener> gardenerOptional = gardenerFormService.findByEmail("a@gmail.com");
+        gardener = gardenerOptional.get();
     }
 
     @Given("I have an image of a plant")
     public void i_have_an_image_of_a_plant() {
-        genericImage = Mockito.mock(MockMultipartFile.class);
+        byte[] imageContent = new byte[]{(byte)0xFF, (byte)0xD8, (byte)0xFF, (byte)0xE0};
+        imageFile = new MockMultipartFile(
+                "image",
+                "test_image.jpg",
+                "image/jpeg",
+                imageContent
+        );
     }
 
-    @When("I upload the image of the plant")
-    public void i_upload_the_image_of_the_plant() throws Exception {
-//        List<String> commonNames = new ArrayList<>(List.of("Common Oak", "English oak", "Carvalho-alvarinho"));
-//        IdentifiedPlant identifiedPlant = new IdentifiedPlant("Quercus robor L.", JsonNode result,
-//            commonNames , testGardener , "/images/plantImageExample/oakTree.jpg");
-        when(genericImage.isEmpty()).thenReturn(false);
-        when(imageService.checkValidImage(any())).thenReturn(Optional.empty());
 
-        IdentifiedPlant identifiedPlant = Mockito.mock(IdentifiedPlant.class);
-        when(plantIdentificationService.identifyPlant(any(),any())).thenReturn(identifiedPlant);
-        when(identifiedPlant.getBestMatch()).thenReturn("Oak");
-        when(identifiedPlant.getScore()).thenReturn(0.4);
-        when(identifiedPlant.getCommonNames()).thenReturn(List.of("Oaky"));
-        when(identifiedPlant.getGbifId()).thenReturn("1234");
-        when(identifiedPlant.getImageUrl()).thenReturn("ImageUrl");
-
-        result =  mockScanControllerMvc.perform(MockMvcRequestBuilders.multipart("/identifyPlant")
-                .file(genericImage)
-                .with(csrf()));
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/gardens/details?gardenId=1"));
-//        assertEquals(result, "yes");
+    @Then("the app displays the name and relevant details after identifying")
+    public void the_app_displays_the_name_and_relevant_details_after_identifying() throws Exception {
+        // Mock testIdentifiedPlant(contains mock plant details) has been defined in MockConfigurationSteps
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/identifyPlant")
+                        .file(imageFile)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.bestMatch").value("Helianthus annuus"))
+                .andExpect(jsonPath("$.score").value(0.88))
+                .andExpect(jsonPath("$.gbifId").value("5414641"))
+                .andExpect(jsonPath("$.imageUrl").value("https://example.com/sunflower.jpg"));
     }
 
-    @Then("the app should accurately identify the plant species")
-    public void the_app_should_accurately_identify_the_plant_species() throws Exception {
-//        result
-//                .andExpect(status().isOk());
-    }
 
-    @Then("display the name and relevant details")
-    public void display_the_name_and_relevant_details() {
 
-    }
+
 
     @When("I upload the image of the plant which cant be identified")
     public void i_upload_the_image_of_the_plant_which_cant_be_identified() throws Exception {
