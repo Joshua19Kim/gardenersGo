7c2b936ae5df9e5db7cea75ae5b0b59d54c6928b
Isaac Steele <ist46@uclive.ac.nz>
2024-08-30 14:29:21 +1200

    As per Sam's comments, I added javadoc and changed the styling so that all the cards are the same height and that the grid's are responsive with change of screen size.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantWikiController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantWikiController.java
@@ -1,15 +1,11 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.WikiPlant;
-import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.PlantWikiService;
-import nz.ac.canterbury.seng302.gardenersgrove.service.TagService;
-import nz.ac.canterbury.seng302.gardenersgrove.util.ValidityChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+/**
+ * The controller for the plant wiki page. It handles all requests for going to the page and searching for plant information.
+ */
 @Controller
 public class PlantWikiController {
 
@@ -36,6 +32,13 @@ public class PlantWikiController {
     }
 
 
+    /**
+     * The main method to get to the plant wiki page
+     * @param model the model which has all the necessary attributes
+     * @return the html template that displays all the plant information
+     * @throws IOException
+     * @throws URISyntaxException
+     */
     @GetMapping("/plantWiki")
     public String plantWiki(
             Model model
@@ -46,6 +49,15 @@ public class PlantWikiController {
         return "plantWikiTemplate";
     }
 
+    /**
+     * The post request when the user searches for the plant information. It queries the API and returns matching plants.
+     * If no plants are found it will display an error message
+     * @param searchTerm the term that the user entered in the search bar
+     * @param model the model which has all the necessary attributes
+     * @return the html template that displays all the plant information
+     * @throws IOException
+     * @throws URISyntaxException
+     */
     @PostMapping("/plantWiki")
     public String plantWikiSearch(@RequestParam("searchTerm") String searchTerm, Model model) throws IOException, URISyntaxException {
 
