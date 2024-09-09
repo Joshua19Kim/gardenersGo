d69b64d75c04f7914957bc72ea8c17e0ea0e3695
Rinlada <rto52@uclive.ac.nz>
2024-09-09 13:44:08 +1200

    Started writing cucumber but got kicked out of lab.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/AddTagFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/AddTagFeature.java
@@ -1,15 +1,25 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+
+import java.util.Objects;
 import java.util.Optional;
+
+import io.cucumber.java.en.When;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Gardener;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Tag;
 import nz.ac.canterbury.seng302.gardenersgrove.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 public class AddTagFeature {
@@ -21,6 +31,7 @@ public class AddTagFeature {
   private Garden garden;
   private Tag tag;
   private Gardener gardener;
+  private MvcResult mvcResult;
 
   @Before("@U21")
   public void setUp() {
@@ -30,27 +41,40 @@ public class AddTagFeature {
     garden = gardenOptional.get();
   }
 
-  @Given("I add an invalid tag")
-  public void i_add_an_invalid_tag() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  @Given("I add a valid tag")
+  public void i_add_a_valid_tag() {
+    tag = new Tag("My tag", garden);
+
+  }
+  @Given("I add an invalid {string}")
+  public void i_add_an_invalid(String tagInput) {
+    tag = new Tag(tagInput, garden);
   }
 
-  @Then("the tag is not added the garden tags")
-  public void the_tag_is_not_added_the_garden_tags() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  @When("I confirm the tag name")
+  public void i_confirm_the_tag_name() throws Exception {
+    mvcResult= mockMvcGardenDetailsController
+            .perform(
+                    (MockMvcRequestBuilders.post("/gardens/addTag")
+                            .param("tag-input", tag.getName())
+                            .param("gardenId", "1")
+                            .with(csrf())))
+            .andReturn();
+  }
+
+  @Then("the tag is not added to the garden tags")
+  public void the_tag_is_not_added_to_the_garden_tags() {
+    assertEquals(tagService.findTagByNameAndGarden(tag.getName(), garden), Optional.empty());
   }
 
   @Then("the tag is added to the garden tags")
   public void the_tag_is_added_to_the_garden_tags() {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    assertEquals(tagService.findTagByNameAndGarden(tag.getName(), garden).get().getName(), tag.getName());
   }
 
   @Then("an error message displays {string}")
   public void an_error_message_says(String errorMessage) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    assertEquals(Objects.requireNonNull(mvcResult.getModelAndView()).getModel().get("tagWarning"), errorMessage);
   }
 }
--- src/test/resources/features/U21-AddingTags.feature
+++ src/test/resources/features/U21-AddingTags.feature
@@ -3,19 +3,21 @@ Feature: U21 - As Inaya, I want to be able to browse gardens by different user-s
   @U21
   Scenario: # AC5 - valid
     Given I am logged in with email "a@gmail.com" and password "Password1!"
-    And I am adding a valid tag
-    When I confirm the tag
+    And I add a valid tag
+    When I confirm the tag name
     Then the tag is added to the garden tags
   @U21
-  Scenario: # AC6 - invalid character
+  Scenario Outline: # AC6,7
     Given I am logged in with email "a@gmail.com" and password "Password1!"
-    And I add an invalid tag
-    When I confirm the tag
-    Then the tag is not added to the list of user-defined tags
-  @U21
-  Scenario: # AC7 - invalid length
-    Given I am logged in with email "a@gmail.com" and password "Password1!"
-    And I add an invalid tag
-    When I confirm the tag
-    Then the tag is not added the garden tags
-    And an error message displays "A tag cannot exceed 25 characters <br/>"
+    And I add an invalid "<tag>"
+    When I confirm the tag name
+    Then the tag is not added to the garden tags
+    And an error message displays <message>
+
+    Examples:
+      | tag                        | message                                                                                                                                                                                                                                 |
+      | ThisTagNameIsWayTooInvalid | "The tag name must only contain alphanumeric characters, spaces, -, _, ', or \" <br/>"                                                                                                                                                  |
+      | Invalid@Tag                | "A tag cannot exceed 25 characters <br/>"                                                                                                                                                                                               |
+      | -                          | "The tag name must contain at least one alphanumeric character"                                                                                                                                                                         |
+      | $$$$                       | "The tag name must only contain alphanumeric characters, spaces, -, _, ', or \" <br/>The tag name must contain at least one alphanumeric character"                                                                                     |
+      | @@@@@@@@@@@@@@@@@@@@@@@@@@ | "The tag name must only contain alphanumeric characters, spaces, -, _, ', or \" <br/>The tag name must only contain alphanumeric characters, spaces, -, _, ', or \" <br/>The tag name must contain at least one alphanumeric character" |
