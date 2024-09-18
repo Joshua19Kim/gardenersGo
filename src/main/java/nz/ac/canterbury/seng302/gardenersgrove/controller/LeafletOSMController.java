14361711d32a91f1508bbce70ddc43293f247933
Kush Desai <kde54@uclive.ac.nz>
2024-09-18 17:02:32 +1200

    Added a map page with clustering!

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LeafletOSMController.java
@@ -0,0 +1,27 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import netscape.javascript.JSObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+/**
+ * Controller for displaying Open Street Maps through JavaFX webview
+ * @author Morgan English
+ */
+@Controller
+public class LeafletOSMController {
+    private static final Logger log = LogManager.getLogger(LeafletOSMController.class);
+//    private JavaScriptBridge javaScriptBridge;
+    private JSObject javaScriptConnector;
+
+
+    @GetMapping("/map")
+    private String map() {
+        return "mapTemplate";
+    }
+
+}
+
