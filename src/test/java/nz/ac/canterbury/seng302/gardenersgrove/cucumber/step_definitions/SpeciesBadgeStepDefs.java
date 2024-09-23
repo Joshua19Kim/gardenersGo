7f6ba1c396ad7800dc639b6c2bd3cc0fe0c1157b
Isaac Steele <ist46@uclive.ac.nz>
2024-09-23 16:13:07 +1200

    Wrote cucumber tests for U7009B and integration tests for the new service methods that I wrote.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/SpeciesBadgeStepDefs.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/SpeciesBadgeStepDefs.java
@@ -21,17 +21,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-public class EarnBadgeFeature {
+public class SpeciesBadgeStepDefs {
 
     @Autowired
     private MockMvc mockMvc;
@@ -47,45 +45,35 @@ public class EarnBadgeFeature {
     @Autowired
     private BadgeService badgeService;
     private ResultActions resultActions;
-    private Gardener testGardener;
+    private Gardener gardener;
 
 
-    @Before("@U7009A")
-    public void setUp() {        badgeRepository.deleteAll();
-    }
-    @After("@U7009A")
+    @Before("@U7009B")
+    public void setUp() {badgeRepository.deleteAll();}
+    @After("@U7009B")
     public void tearDown() {
         identifiedPlantRepository.deleteAll();
         badgeRepository.deleteAll();
     }
 
-    @Given("I have collected {int} plants,")
-    public void i_have_collected_plants(Integer initialPlantCount) {
-        testGardener = gardenerFormService.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName()).get();
+    @Given("I have collected {int} species,")
+    public void i_have_collected_species(Integer speciesCount) {
+        gardener = gardenerFormService.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName()).get();
 
-        for(int i = 0; i <initialPlantCount; i++) {
-            IdentifiedPlant plant = new IdentifiedPlant("Plant " +i,
-                    0.88,
-                    List.of("Sunflower", "Rose"),
-                    "5414641",
-                    "https://example.com/sunflower.jpg",
-                    "https://example.com/sunflower.jpg",
-                    "Helianthus",
-                    "annuus",
-                    testGardener);
+        for(int i = 0; i <speciesCount; i++) {
+            IdentifiedPlant plant = new IdentifiedPlant("Plant " +i, gardener);
+            plant.setSpeciesScientificNameWithoutAuthor("Species" + i);
             identifiedPlantService.saveIdentifiedPlantDetails(plant);
         }
-
-
     }
-    @When("I have collected another plant")
-    public void i_have_collected_my_plant() throws Exception {
+    @When("I have collected another species")
+    public void i_have_collected_another_species() throws Exception {
         MockMultipartFile imageFile = new MockMultipartFile("plantImage", "image.jpg", "image/jpeg", new byte[0]);
         resultActions = mockMvc.perform(MockMvcRequestBuilders.multipart("/myCollection")
                         .file(imageFile)
-                        .param("plantName", "Final Plant" )
-                        .param("description", "Plant description")
-                        .param("scientificName",  "scientific name")
+                        .param("plantName", "New Plant" )
+                        .param("description", "Awesome description")
+                        .param("scientificName",  "Sick species")
                         .param("uploadedDate", String.valueOf(LocalDate.parse("12/02/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy"))))
                         .param("isDateInvalid", "false")
                         .with(csrf()))
@@ -93,11 +81,13 @@ public class EarnBadgeFeature {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/myCollection"));
     }
-    @Then("I will be shown the plant badge with name {string}")
-    public void i_will_be_shown_the_plant_badge_with_name(String expectedName) throws Exception {
-        Optional<Badge> badge = badgeService.getMyBadgeByName(expectedName, testGardener.getId());
+    @Then("I will be shown the species badge with name {string}")
+    public void i_will_be_shown_the_species_badge_with_name(String speciesName) throws Exception {
+        Optional<Badge> badge = badgeService.getMyBadgeByName(speciesName, gardener.getId());
         Assertions.assertTrue(badge.isPresent());
-        Assertions.assertEquals(expectedName, badge.get().getName());
-        resultActions.andExpect(flash().attributeExists("plantBadge"));
+        Assertions.assertEquals(speciesName, badge.get().getName());
+        resultActions.andExpect(flash().attributeExists("speciesBadge"));
+
     }
+
 }
